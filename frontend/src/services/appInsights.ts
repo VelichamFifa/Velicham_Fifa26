aad1da36d3253999c5e1bb1a869298ac362a5398
abduldm <abduldm@gmail.com>
2026-06-22 22:59:20 -0400

    Update appInsights.ts

--- frontend/src/services/appInsights.ts
+++ frontend/src/services/appInsights.ts
@@ -1,7 +1,7 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
 const fallbackConnectionString =
-  'InstrumentationKey=InstrumentationKey=9675ba98-99f5-4244-922f-5775c203e74d;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=c146dcdc-902e-4061-911b-43748d1199d6';
+  'InstrumentationKey=9675ba98-99f5-4244-922f-5775c203e74d;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=c146dcdc-902e-4061-911b-43748d1199d6';
 const fallbackRoleName = 'fifa26-frontend-qa';
 
 const connectionString = import.meta.env.VITE_APPINSIGHTS_CONNECTION_STRING || fallbackConnectionString;
